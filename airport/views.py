5b276261f69be122640b3a7f860cc2eac07da0c7
Mariana Lishchynska <lishchynska.m@gmail.com>
2025-02-21 09:11:23 +0200

    initialize view for airplane

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from airport.models import Airplane
+from airport.serializers import AirplaneSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["name", "total_number_of_seats"]
+    ordering = ["name"]
