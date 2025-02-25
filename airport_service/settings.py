e4d32cba0cf648c7ec0d8805500624d6b9f4244e
Mariana Lishchynska <lishchynska.m@gmail.com>
2025-02-25 09:19:51 +0200

    adjust code style for project configuration according flake8

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -76,16 +76,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".NumericPasswordValidator",
     },
 ]
 
@@ -119,7 +123,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "3/min", "user": "20/min"},
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/min", "user": "30/min"},
 }
 
 SIMPLE_JWT = {
--- airport_service/urls.py
+++ airport_service/urls.py
@@ -17,7 +17,7 @@ urlpatterns = (
         path("user/", include("user.urls")),
         path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
         path(
-            "api/v1/schema/swagger",
+            "api/v1/doc/swagger",
             SpectacularSwaggerView.as_view(url_name="schema"),
             name="swagger-ui",
         ),
